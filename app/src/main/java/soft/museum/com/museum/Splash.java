ffaabf050e2e9dbad7a334f9ce45ae766778e04f
purashichiyo <purashichiyo@gmail.com>
2018-10-12 21:58:53 +0800

    repair 2

---
+++ app/src/main/java/soft/museum/com/museum/Splash.java
@@ -0,0 +1,29 @@
+package soft.museum.com.museum;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.content.Intent;
+
+
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent();
+                intent.setClass(Splash.this,
+                        Home.class);
+
+                Splash.this.startActivity(intent);
+                Splash.this.finish();
+            }
+        }, 5000);
+    }
+}
